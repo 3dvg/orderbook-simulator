0cad6d672b1de1e03841f8d2ce6e27cebe9330bd
Eduardo Gallego <3dvgg1@gmail.com>
2022-10-04 14:10:19 +0200

    hide order simulations

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     // let vals = gbm::generate_gbm(100.0, 1.0 / 31_536_000.0, 10, 0.15, 0.5);
     // println!("vals {:?}", vals);
-    let mut wtr = Writer::from_path("orders.csv")?;
+    let mut wtr = Writer::from_path("./order_simulations/orders.csv")?;
     while let Ok(message) = receiver.recv().await {
         // println!("{:?}", message);
         wtr.serialize(message)?;
